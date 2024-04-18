d2a57bac5ff1fbb8ab4f2eeef60111971906d5c4
Charlotteg1 <charlotte.gould3@gmail.com>
2024-04-19 00:17:49 +0100

    added methods for adding and deleting accomodation

--- tripPlannerAppBackend/src/main/java/com/tripPlanner/demo/models/dtos/AccommodationDTO.java
+++ tripPlannerAppBackend/src/main/java/com/tripPlanner/demo/models/dtos/AccommodationDTO.java
@@ -1,55 +1,39 @@
-package com.tripPlanner.demo.models;
+package com.tripPlanner.demo.models.dtos;
 
+import com.tripPlanner.demo.models.Day;
 import com.tripPlanner.demo.models.enums.AccommodationType;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@Entity
-@Table(name = "accommodations")
-public class Accommodation {
+public class AccommodationDTO {
+    private List<Long> dayIds;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @OneToMany(mappedBy = "accommodation")
-    private List<Day> days;
-
-    @Column
     private String name;
 
-    @Enumerated(EnumType.STRING)
     private AccommodationType type;
 
-    @Column
     private String bookingDetails;
-    //act as notes for the user, so they can store a booking reference, check in time etc
 
-    @Column
     private String address;
 
-    public Accommodation(List<Day> days, String name, AccommodationType type, String bookingDetails, String address) {
-        this.days = days;
+    public AccommodationDTO(List<Long> dayIds, String name, AccommodationType type, String bookingDetails, String address) {
+        this.dayIds = dayIds;
         this.name = name;
         this.type = type;
         this.bookingDetails = bookingDetails;
         this.address = address;
     }
 
-    public Accommodation() {
+    public List<Long> getDayIds() {
+        return dayIds;
     }
 
-    public Long getId() {
-        return id;
+    public void setDayIds(List<Long> dayIds) {
+        this.dayIds = dayIds;
     }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-
 
     public String getName() {
         return name;
--- tripPlannerAppBackend/src/main/java/com/tripPlanner/demo/services/AccommodationService.java
+++ tripPlannerAppBackend/src/main/java/com/tripPlanner/demo/services/AccommodationService.java
@@ -1,9 +1,16 @@
 package com.tripPlanner.demo.services;
 
 
+import com.tripPlanner.demo.models.Accommodation;
+import com.tripPlanner.demo.models.Day;
+import com.tripPlanner.demo.models.dtos.AccommodationDTO;
 import com.tripPlanner.demo.repostitories.AccommodationRepository;
+import com.tripPlanner.demo.repostitories.DayRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AccommodationService {
@@ -14,4 +21,24 @@ public class AccommodationService {
 
     @Autowired
     AccommodationRepository accommodationRepository;
+
+    @Autowired
+    DayRepository dayRepository;
+
+    public Accommodation addAccommodation(AccommodationDTO accommodationDTO){
+        List<Day> days = new ArrayList<>();
+        for( Long dayId :accommodationDTO.getDayIds() ){
+            Day day = dayRepository.findById(dayId).get();
+            days.add(day);
+        }
+        Accommodation newAccommodation = new Accommodation(days, accommodationDTO.getName(), accommodationDTO.getType(), accommodationDTO.getBookingDetails(), accommodationDTO.getAddress());
+        return accommodationRepository.save(newAccommodation);
+
+//       need to check that is added on the Day side
+    }
+
+    public void deleteAccommodation(Long accommodationId){
+        accommodationRepository.deleteById(accommodationId);
+    }
+    
 }
