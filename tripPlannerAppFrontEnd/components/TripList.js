39229f8765a3cc018408307b0ba31643fb1399ae
Charlotteg1 <charlotte.gould3@gmail.com>
2024-04-29 22:52:36 +0100

    fetch for users trips

--- tripPlannerAppFrontEnd/components/TripList.js
+++ tripPlannerAppFrontEnd/components/TripList.js
@@ -1,24 +1,60 @@
-import { Button, SafeAreaView , Text} from "react-native"
+import { SafeAreaView , Text, Pressable, StyleSheet} from "react-native"
 import { useState } from "react"
 
 const TripList = ({navigation}) =>{
 
+    const [trips, setTrips] = useState()
     const [currentTripId, setCurrentTripId] = useState()
-    const [futureTripIds, setFutureTripIds] = useState([])
-    const [pastTripIds, setPastTripIds] = useState([])
+    const [futureTripIds, setFutureTripIds] = useState()
+    const [pastTripIds, setPastTripIds] = useState()
+    const [currentUserId, setCurrentUserId] = useState(2);
+
+    const fetchTrips = async () => {
+        const url = `http://localhost:8080/user/authenticate/user/${currentUserId}`;
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if(response.status === 302){
+            setTrips(data);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    useEffect(()=>{
+      fetchTrips();
+    },[]);
+
 
 
     return(
     <SafeAreaView>
-        <Button onPress={()=>navigation.navigate('addTrips')} title="Add Trip"/>
-        <Text>View future, current and past trips here</Text>
-        {/* add trip option, directs to AddTrip screen*/}
-        {/* fetch all trips, search to find current (if) going on against the current date */}
+        <Pressable style={styles.addTrip} onPress={()=>navigation.navigate('addTrips')}>
+            <Text style={[styles.addTripText, { textAlign: 'center' }]}>Add Trip</Text>
+        </Pressable>
+        {currentTripId && futureTripIds &&  pastTripIds && <Text>Loading...</Text>} 
+        {/* check below if equals null  */}
+        {currentTripId && futureTripIds &&  pastTripIds && <Text>You have no trips please add trip to display</Text>} 
         {currentTripId && <Text>Current Trip</Text>}
-        {/* current trip displays , (call trip component)*/}
         {futureTripIds && <Text>Future Trips</Text>}
-        {/* future trip displays , in  reverse chronological order , call trip component*/}
         {pastTripIds && <Text>Past Trips</Text>}
     </SafeAreaView>)
-}
+    }
+
+const styles = StyleSheet.create({
+    addTrip: {
+        alignSelf: 'center',
+        marginTop: 2,
+        backgroundColor: '#80DBA1',
+        padding: 8,
+        borderRadius:10,
+        width: 100,
+        shadowOpacity: '0.2%',
+        shadowColor: '#775204',
+    },
+    addTripText: {
+        fontFamily: 'Courier New'
+    },
+});
 export default TripList;
